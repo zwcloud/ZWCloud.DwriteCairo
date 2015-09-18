1029d3f700ffae2a163c712fccf5b00aead6b3c8
ZCloud <zwcloud@yeah.net>
2015-09-18 18:26:17 +0800

    Add drawing methods to demo
    Custom cairo path can be rendered normally but DWriteCairo.ShowLayout
    doesn't work.

--- DwriteCairoDemo/Form1.Designer.cs
+++ DwriteCairoDemo/Form1.Designer.cs
@@ -28,57 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(26, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(224, 98);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(26, 27);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(224, 21);
-            this.textBox2.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 12);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "GUID";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(384, 262);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.DoubleClick += new System.EventHandler(this.button1_Click);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
+
     }
 }
 
--- DwriteCairoDemo/Form1.cs
+++ DwriteCairoDemo/Form1.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Cairo;
 using ZWCloud.DWriteCairo;
+using ZWCloud.DWriteCairo.DWrite;
+using ZWCloud.DWriteCairo;
+using ZWCloud.DWriteCairo.DWrite;
 using FontStyle = ZWCloud.DWriteCairo.FontStyle;
+using FontWeight = ZWCloud.DWriteCairo.FontWeight;
+using Graphics = System.Drawing.Graphics;
 
 namespace DWriteCairoDemo
 {
     public partial class Form1 : Form
     {
+        public Graphics Graphics1 { get; private set; }
+        public Context Context1 { get; set; }
+        public Win32Surface Surface1 { get; private set; }
+
+        private bool ready;
+        private TextLayout textLayout;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr GetDC(IntPtr hWnd);
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -21,13 +38,44 @@ namespace DWriteCairoDemo
                 FontStyle.DWRITE_FONT_STYLE_NORMAL,
                 FontStretch.DWRITE_FONT_STRETCH_NORMAL,
                 32f);
-
+            
             Debug.Assert(Math.Abs(textFormat.FontSize - 32f) < 0.0001);
-
+            
             const string s = "Hello World";
-            var textLayout = DWriteCairo.CreateTextLayout(s, s.Length, textFormat, 300, 40);
+            textLayout = DWriteCairo.CreateTextLayout(s, s.Length, textFormat, 300, 40);
+            ready = true;
+        }
 
-            //DWriteCairo.ShowLayout();
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            var p1 = new PointD(10, 10);
+            var p2 = new PointD(100, 10);
+            var p3 = new PointD(100, 100);
+            var p4 = new PointD(10, 100);
+
+            Context1.SetSourceColor(new Cairo.Color(1,0,0));
+            Context1.MoveTo(p1);
+            Context1.LineTo(p2);
+            Context1.LineTo(p3);
+            Context1.LineTo(p4);
+            Context1.LineTo(p1);
+            Context1.ClosePath();
+            Context1.Stroke();
+
+            if (ready)
+            {
+                Debug.WriteLine("showing layout");
+                DWriteCairo.ShowLayout(Context1, textLayout);
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Surface1 = new Win32Surface(this.CreateGraphics().GetHdc());
+            Context1 = new Context(Surface1);
+            Context1.Antialias = Antialias.Subpixel;
         }
     }
 }
