b2bed727f386011d53e980ab0db0cc73c259ea40
ZCloud <zwcloud@yeah.net>
2015-09-22 00:24:37 +0800

    Remove extra project configurations.
    Writing unit test.

---
+++ DWriteCairoTest/UnitTestTextLayout.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZWCloud.DWriteCairo;
+
+
+namespace DWriteCairoTest
+{
+    [TestClass]
+    public class UnitTestDWriteCairo
+    {
+        [TestMethod]
+        public void CreateTextFormat()
+        {
+            const string fontFamilyName = "SimSun";
+            const FontWeight fontWeight = FontWeight.Bold;
+            const FontStyle fontStyle = FontStyle.Normal;
+            const FontStretch fontStretch = FontStretch.Normal;
+            const float fontSize = 32f;
+
+            var textFormat = DWriteCairo.CreateTextFormat(fontFamilyName, fontWeight, fontStyle, fontStretch, fontSize);
+
+            Assert.IsNotNull(textFormat, "TextFormat creating failed.");
+        }
+
+        [TestMethod]
+        public void TestTextFormatApi()
+        {
+            const string fontFamilyName = "SimSun";
+            const FontWeight fontWeight = FontWeight.Bold;
+            const FontStyle fontStyle = FontStyle.Normal;
+            const FontStretch fontStretch = FontStretch.Normal;
+            const float fontSize = 32f;
+
+            var textFormat = DWriteCairo.CreateTextFormat(fontFamilyName, fontWeight, fontStyle, fontStretch, fontSize);
+
+
+            Assert.AreEqual(fontSize, textFormat.FontSize);
+        }
+    }
+}
